1b86cd289ff0bb3f9ff09f9aa1b507b9a00497b8
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-04-22 13:01:42 +0500

    213
    312

--- src/pages/host.js
+++ src/pages/host.js
@@ -1,5 +1,5 @@
 // var url="https://dastafka-back.onrender.com"
 // var url="http://siriyuuc.beget.tech"
 //  var url="http://localhost:4004"
-var url='https://fre.abbas.uz'
+var url='https://test.codeup.uz'
 module.exports=url
