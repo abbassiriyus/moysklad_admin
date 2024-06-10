70ef7622194518e42ad96eb2620fdba9346389cd
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-06-10 11:23:54 +0500

    345
    5

--- src/pages/host.js
+++ src/pages/host.js
@@ -1,5 +1,5 @@
 // var url="https://dastafka-back.onrender.com"
 // var url="http://siriyuuc.beget.tech"
 //  var url="http://localhost:4004"
-var url='https://test.codeup.uz'
+var url='https://mysklad-back.onrender.com'
 module.exports=url
