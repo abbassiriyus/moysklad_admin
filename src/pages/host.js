f971965be4e8bea9e2729bd53ea79f38941c8d05
otabekbeb <86982819+abbassiriyus@users.noreply.github.com>
2025-01-28 23:08:58 +0500

    345
    345

--- src/pages/host.js
+++ src/pages/host.js
@@ -1,5 +1,5 @@
 // var url="https://dastafka-back.onrender.com"
 // var url="http://localhost:4004"
- var url="https://api.abbas.uz"
+ var url="https://api.rce.uz"
 // var url='https://mysklad-back.onrender.com'
 module.exports=url
